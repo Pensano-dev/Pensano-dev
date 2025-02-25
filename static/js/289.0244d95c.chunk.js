2c122ed38a1f027e67fccb5d5004c9ccd7e807d3
Juliana Nocchi Dobal <junocchi@gmail.com>
2025-02-25 20:10:19 +0000

    Updates

---
+++ static/js/289.0244d95c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpensano=self.webpackChunkpensano||[]).push([[289],{289:function(e){e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=289.0244d95c.chunk.js.map
