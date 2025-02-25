642e2fcd8b3cbdbd23e101a116d1d449c115de18
Juliana Nocchi Dobal <junocchi@gmail.com>
2025-02-25 19:39:29 +0000

    feat: update project descriptions for Aura, Meal Plan, and Pensano Devs website with latest details

--- src/components/OurProjects/OurProjects.js
+++ src/components/OurProjects/OurProjects.js
@@ -14,107 +14,116 @@ export default function OurProjects() {
         {/* Project AURA */}
         <div className="our-projects-individual-containers">
           <div className="project-textAll">
-            <h3>🌈 Aura (🚧 under construction)</h3>
+            <h3 style={{ marginTop: 10 }}>
+              🌈 Aura{" "}
+              <a
+                href="https://www.canva.com/design/DAFnaNSUH6c/3HwneJ2_axm4dUbXObnFIQ/view?utm_content=DAFnaNSUH6c&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink#1"
+                target="_blank"
+              >
+                (🚧 in progress)
+              </a>
+            </h3>
+
             <p>
-              The idea was intially formed at the Athena Hackathon 2023 by Elsa,
-              Ju, Natasha, Sidra and Tian, which was awarded both the Cisco
-              prize: "Empowering Accessibility & Inclusion Through Innovation”
-              and the Nationwide prize: “Highest Potential Impact”. This mobile
-              app hopes to support social inclusion by helping users find venues
-              catered to their specific needs, whether related to physical
+              Aura is a mobile app that was conceived at the Athena Hackathon
+              2023 by Elsa, Ju, Natasha, Sidra, and Tian, where it won both the
+              Cisco prize for "Empowering Accessibility & Inclusion Through
+              Innovation" and the Nationwide prize for "Highest Potential
+              Impact." The app aims to promote social inclusion by helping users
+              discover venues that meet their specific needs, such as physical
               accessibility, neurological considerations, or creating the right
-              ambience.
+              ambience. By offering tailored information, Aura empowers users to
+              explore spaces that address their individual requirements.
               <br></br>
-              <p style={{ fontSize: 14 }}>
-                ⚙️ Tech: Figma, React Native and GitHub.
+              <p style={{ fontSize: 14, marginTop: 10 }}>
+                ⚙️ Tech: Google Maps API, NodeJS, React Native, and Supabase.
               </p>
             </p>
-              <a href={"https://github.com/Pensano-dev/aura-mobile-app"} target="_blank" rel="noopener noreferrer">
-                <FaGithub className="github-icon" />
-              </a>
+            <a
+              href={"https://github.com/Pensano-dev/aura-new"}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub className="github-icon" />
+            </a>
           </div>
         </div>
 
         {/* Project Meal plan generator */}
         <div className="our-projects-individual-containers">
           <div className="project-textAll">
-            <h3>🥙 7-day Meal Plan AI generated (🚧 under construction)</h3>
+            <h3 style={{ marginTop: 10 }}>
+              🥙 7-day Meal Plan AI generated{" "}
+              <a
+                href="https://almost-meal-planner.onrender.com/"
+                target="_blank"
+              >
+                (🚧 in progress)
+              </a>
+            </h3>
             <p>
-              This web app is being developed to support Nutritional Therapy
+              Meal Plan is a web app designed to assist Nutritional Therapy
               practitioners and students from{" "}
               <a href="https://www.newschoolofnutrition.com/" target="_blank">
                 The New School of Nutritional Medicine.
               </a>{" "}
-              After adding their client's preferences, allergies and/or
-              intolerances, the program generates a 7-day meal plan that can be
-              downloaded; leaving more time to the practitioners to focus on
-              providing personalised recommendations for their patients.
+              By allowing users to input their client's dietary preferences,
+              allergies, and intolerances, the app generates a personalized
+              7-day meal plan that can be easily downloaded. This automation
+              streamlines the planning process, freeing up more time for
+              practitioners to focus on delivering tailored nutritional advice
+              and personalized recommendations for their patients, enhancing
+              both efficiency and client care.
               <br></br>
-              <p style={{ fontSize: 14 }}>
-                ⚙️ Tech: MongoDB, Javascript, Jest, Cypress, HTML, CSS,
-                Git, and GitHub.
+              <p style={{ fontSize: 14, marginTop: 10 }}>
+                ⚙️ Tech: MongoDB, NodeJS, Jest, Cypress, HTML, CSS, and ChatGPT
+                API.
               </p>
             </p>
-              <a href={"https://github.com/Pensano-dev/meal-plan-generator"} target="_blank" rel="noopener noreferrer">
-                <FaGithub className="github-icon" />
-              </a>
-          </div>
-        </div>
-
-        {/* Project HTMA report */}
-        <div className="our-projects-individual-containers">
-          <div className="project-textAll">
-            <h3>🧪 HTMA Report Generator (🚧 under construction)</h3>
-            <p>
-              This online, login-free, program is being developed to support
-              Nutritional Therapy practitioners and students from{" "}
-              <a href="https://www.newschoolofnutrition.com/" target="_blank">
-                The New School of Nutritional Medicine
-              </a>{" "}
-              analysing Hair Tissue Mineral Analysis results. After adding the
-              test results, the program generates a comprehensive report that
-              can be downloaded; leaving more time to the practitioners to focus
-              on providing personalised recommendations for their patients.
-              <br></br>
-              <p style={{ fontSize: 14 }}>
-                ⚙️ Tech: React, Javascript, HTML, CSS, Chat GPT, Figma,
-                Git, and GitHub.
-              </p>
-            </p>
+            <a
+              href={"https://github.com/Pensano-dev/meal-plan-generator"}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub className="github-icon" />
+            </a>
           </div>
         </div>
 
         {/* Project Pensano */}
         <div className="our-projects-individual-containers">
           <div className="project-textAll">
-            <h3>💡 Pensano Devs Website</h3>
+            <h3 style={{ marginTop: 10 }}>💡 Pensano Devs Website</h3>
             <p>
-              Our very first project working as Pensano Devs was to developed
-              this website to showcase our work. It focuses on mobile
-              responsiveness and has a consistent colour scheme and typography
-              throughout. Thanks to the use of Excalidraw and Canva, we were
-              able to create detailed diagrams and prototypes that helped guide
-              the site's design and layout. We also used Trello and Slack to
-              track progress and communicate with one another. This is a
-              front-end project, built with React and CSS.
+              The Pensano Devs website was our very first project as a team,
+              created to showcase our work. We focused on mobile responsiveness
+              and ensured a consistent color scheme and typography throughout
+              the site. Over the years, many volunteers have come and gone,
+              which led to continuous updates and improvements to keep the site
+              aligned with our evolving team and projects. Now in its third
+              version, this front-end project, built using React and CSS,
+              reflects our collaborative approach and dedication to creating a
+              polished, user-friendly experience.
               <br></br>
-              <p style={{ fontSize: 14 }}>
-                ⚙️ Tech: Git, GitHub, JavaScript, React, Cypress, CSS and
-                HTML.
+              <p style={{ fontSize: 14, marginTop: 10 }}>
+                ⚙️ Tech: NodeJS, React, and CSS.
               </p>
             </p>
-            <a href={"https://github.com/Pensano-dev/Pensano-dev"} target="_blank" rel="noopener noreferrer">
-            <FaGithub className="github-icon" />
+            <a
+              href={"https://github.com/Pensano-dev/Pensano-dev"}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub className="github-icon" />
             </a>
           </div>
         </div>
       </div>
 
       {/* Footer */}
-        <div className="footer-container">
-          <p className="footer-text-copyright">© Pensano Developers 2025</p>
-        </div>
-
+      <div className="footer-container">
+        <p className="footer-text-copyright">© Pensano Developers 2025</p>
+      </div>
     </IconContext.Provider>
   );
 }
